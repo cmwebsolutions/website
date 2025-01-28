566b09e14ef07079e26dfc3bbe812ea2980fbccc
ciaran-cmws <ciaran@cmwebsolutions.com>
2025-01-28 18:29:30 +0000

    Change colours in config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'black': '#1B1919',
+				'white': '#FCFCFC',
+			}
+		},
 	},
 	plugins: [],
 }
