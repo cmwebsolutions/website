c65e6684674a3891c34a196378ee7399c2cd1bfb
Ciaran Murray <ciaranmurray@Ciarans-MacBook-Air.local>
2026-07-02 12:38:22 +0100

    Setup a sitemap for the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cmwebsolutions.com',
-  integrations: [mdx()]
+  site: 'https://www.cmwebsolutions.com',
+  integrations: [mdx(), sitemap()]
 });
